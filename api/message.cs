a97b3e730eb593ce8fb29d413e9c557efed2bd8c
Matt <macavall@microsoft.com>
2025-07-24 11:30:08 -0500

    first

---
+++ api/message.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace swatest560113func;
+
+public class message
+{
+    private readonly ILogger<message> _logger;
+
+    public message(ILogger<message> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("message")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+    }
+}
